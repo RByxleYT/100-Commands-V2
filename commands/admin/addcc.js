5e82a2cd75179aea73a5a4c836b541b59c1f29f9
RByxleYT <88121705+RByxleYT@users.noreply.github.com>
2021-09-06 06:46:25 +0700

    Update addcc.js

--- commands/admin/addcc.js
+++ commands/admin/addcc.js
@@ -11,20 +11,20 @@ module.exports = {
   run: async (client, message, args) => {
 
 
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(":x: You need `MANAGE_MESSAGES` perms to use this command")
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<:Decline:864080227738320896> You need `MANAGE_MESSAGES` perms to use this command")
 
     let cmdname = args[0];
 
-    if(!cmdname) return message.channel.send(`:x: You have to give command name, \`addcmd <cmd_name> <cmd_responce>\`
+    if(!cmdname) return message.channel.send(`<:Decline:864080227738320896> You have to give command name, \`addcmd <cmd_name> <cmd_responce>\`
     `);
 
     let cmdresponce = args.slice(1).join(" ");
 
-    if(!cmdresponce) return message.channel.send(`:x: You have to give command cmd responce, \`addcmd <cmd_name> <cmd_responce>\`\n\n**You can add the following variables - \n\`{user}\` - Author\n\`{user_tag}\` - Author Tag\n\`{user_name}\` - Author Username\n\`{user_ID}\` - Author ID\n\`{guild_name}\` - Guild Name\n\`{guild_ID}\` - Guild Id\n\`{memberCount}\` - Member Count\n\`{member_createdAtAgo}\` - Member Creation Time\n\`{member_createdAt}\` - Member Creation Date`);
+    if(!cmdresponce) return message.channel.send(`<:Decline:864080227738320896> You have to give command cmd responce, \`addcmd <cmd_name> <cmd_responce>\`\n\n**You can add the following variables - \n\`{user}\` - Author\n\`{user_tag}\` - Author Tag\n\`{user_name}\` - Author Username\n\`{user_ID}\` - Author ID\n\`{guild_name}\` - Guild Name\n\`{guild_ID}\` - Guild Id\n\`{memberCount}\` - Member Count\n\`{member_createdAtAgo}\` - Member Creation Time\n\`{member_createdAt}\` - Member Creation Date`);
 
     let database = db.fetch(`cmd_${message.guild.id}`)
 
-    if(database && database.find(x => x.name === cmdname.toLowerCase())) return message.channel.send(":x: This command name is already added in guild custom commands.")
+    if(database && database.find(x => x.name === cmdname.toLowerCase())) return message.channel.send("<:Decline:864080227738320896> This command name is already added in guild custom commands.")
 
     let data = {
       name: cmdname.toLowerCase(),
