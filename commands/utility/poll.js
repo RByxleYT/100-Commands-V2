2b05c275dc71a7cda32b46939799a9acd706fd5c
RByxleYT <88121705+RByxleYT@users.noreply.github.com>
2021-09-06 09:07:09 +0700

    Update poll.js

--- commands/utility/poll.js
+++ commands/utility/poll.js
@@ -15,14 +15,14 @@ module.exports = {
             return message.channel.send("**Please Enter A Query!**");
 
         const embed = new MessageEmbed()
-            .setColor("GREEN")
+            .setColor('45BB8A')
             .setTitle(`Poll For ${message.guild.name} Sever`)
             .setFooter(message.member.displayName, message.author.displayAvatarURL())
             .setDescription(args.join(' '))
         var msg = await message.channel.send(embed);
 
-        await msg.react('✅');
-        await msg.react('❌');
+        await msg.react('<:Accepted:864080263172718603>');
+        await msg.react('<:Decline:864080227738320896>');
 
         message.delete({ timeout: 1000 });
     }
