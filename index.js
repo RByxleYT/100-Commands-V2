57e61e90b40cd35ce210a0b789e36685cb7858bb
RByxleYT <88121705+RByxleYT@users.noreply.github.com>
2021-09-06 09:28:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 console.log("Testing");
 //Defining dependencies
 const { Client, Collection } = require('discord.js');
-const { PREFIX } = require('./config.js');
+const { PREFIX, TOKEN } = require('./config');
 const discord = require("discord.js");
 const moment = require("moment");
 const ms = require('ms');
