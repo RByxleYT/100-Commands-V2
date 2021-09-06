b9c32d0ec07290226411891eea3b623dde3189ee
RByxleYT <88121705+RByxleYT@users.noreply.github.com>
2021-09-06 09:34:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'ODc4MTY2NTQxNjMwMzI0Nzg3.YR9OUw.ewmoybp92nXdDTerYO61Wjm3QKY';
+exports.token = 'ODc4MTY2NTQxNjMwMzI0Nzg3.YR9OUw.ewmoybp92nXdDTerYO61Wjm3QKY';
 exports.PREFIX = "Helper, ";
 exports.OWNER_ID = "746275512145674351";
 exports.Owner_Name = "RByxle#9999";
